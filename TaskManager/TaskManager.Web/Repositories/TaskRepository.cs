6705f726b980f5215907692a22e3ea067f1b314c
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-05-24 15:01:58 -0300

    Add TaskRepository

---
+++ TaskManager/TaskManager.Web/Repositories/TaskRepository.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskManager.Web.Repositories.Contracts;
+
+namespace TaskManager.Web.Repositories
+{
+    public class TaskRepository : ITaskRepository
+    {
+        private readonly string _connectionString;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public TaskRepository(IConfiguration configuration, ICategoryRepository categoryRepository)
+        {
+            _connectionString = configuration["TaskManager:ConnectionString"];
+            _categoryRepository = categoryRepository;
+        }
+
+        public bool CreateTask(Models.Task task)
+        {
+        }
+    }
+}
