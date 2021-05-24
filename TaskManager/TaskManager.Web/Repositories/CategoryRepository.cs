0f1bb01b7bfaab4756015748353bd90d1aa8af8e
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-05-24 14:23:30 -0300

    Add CategoryRepository

---
+++ TaskManager/TaskManager.Web/Repositories/CategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskManager.Web.Models;
+using TaskManager.Web.Repositories.Contracts;
+
+namespace TaskManager.Web.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly string _connectionString;
+
+        public int CreateCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
