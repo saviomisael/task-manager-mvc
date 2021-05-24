9be7441d5b06ca15a563ce732256817858455468
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-05-24 14:12:46 -0300

    Add model Task

---
+++ TaskManager/TaskManager.Web/Models/Task.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TaskManager.Web.Models
+{
+    public class Task
+    {
+        public int TaskID { get; set; }
+        public int CategoryFK { get; set; }
+        public string NameTask { get; set; }
+        public int PriorityTask { get; set; }
+        public string DescriptionTask { get; set; }
+        public DateTime DateTask { get; set; }
+    }
+}
