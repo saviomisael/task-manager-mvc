6e2438faede686c772a3e15d0eec4186b1dfd182
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-05-24 14:52:30 -0300

    Add ITaskRepository

---
+++ TaskManager/TaskManager.Web/Repositories/Contracts/ITaskRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TaskManager.Web.Models;
+
+namespace TaskManager.Web.Repositories.Contracts
+{
+    interface ITaskRepository
+    {
+        bool CreateTask(Task task);
+    }
+}
