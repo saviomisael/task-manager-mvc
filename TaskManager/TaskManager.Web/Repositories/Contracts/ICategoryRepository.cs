16014d640e7853ab988b8903f2d781e413cab47e
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-05-24 14:21:56 -0300

    Add ICategoryRepository

---
+++ TaskManager/TaskManager.Web/Repositories/Contracts/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskManager.Web.Models;
+
+namespace TaskManager.Web.Repositories.Contracts
+{
+    interface ICategoryRepository
+    {
+        int CreateCategory(Category category);
+    }
+}
