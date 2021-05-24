9f2228e1580124b1a89ac9c0837ad211afd9c38b
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-05-24 15:46:43 -0300

    Add Priority.cs

---
+++ TaskManager/TaskManager.Web/Common/Priority.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace TaskManager.Web.Common
+{
+    public class Priority
+    {
+        public int PriorityId { get; set; }
+        public string NamePriority { get; set; }
+
+        public static IEnumerable<Priority> ReturnPrioritiesForViewModel()
+        {
+            var priorities = new List<Priority>();
+
+            priorities.Add(new Priority()
+            {
+                PriorityId = 1,
+                NamePriority = "Pequena"
+            });
+
+            priorities.Add(new Priority()
+            {
+                PriorityId = 2,
+                NamePriority = "Média"
+            });
+
+            priorities.Add(new Priority()
+            {
+                PriorityId = 3,
+                NamePriority = "Grande"
+            });
+
+            return priorities;
+        }
+    }
+}
