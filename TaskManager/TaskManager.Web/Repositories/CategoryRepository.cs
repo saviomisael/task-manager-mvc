0dd1ef2ac453703f9d7be73d9ee9265dea236815
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-05-24 14:41:30 -0300

    Add connection string for CategoryRepository

--- TaskManager/TaskManager.Web/Repositories/CategoryRepository.cs
+++ TaskManager/TaskManager.Web/Repositories/CategoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,6 +11,11 @@ namespace TaskManager.Web.Repositories
     public class CategoryRepository : ICategoryRepository
     {
         private readonly string _connectionString;
+
+        public CategoryRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration["TaskManager:ConnectionString"];
+        }
 
         public int CreateCategory(Category category)
         {
