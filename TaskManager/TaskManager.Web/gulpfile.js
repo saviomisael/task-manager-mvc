c3e3cd8d5ddd337f10067e8d3ec98333166a4816
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-05-25 15:09:23 -0300

    Add tasks to minify css in gulpfile.js

--- TaskManager/TaskManager.Web/gulpfile.js
+++ TaskManager/TaskManager.Web/gulpfile.js
@@ -1,3 +1,18 @@
 ﻿const { series, src, dest, watch } = require("gulp");
 const cleanCSS = require("gulp-clean-css");
+const rename = require("gulp-rename");
 const gulpRename = require("gulp-rename");
+
+const minifyCSS = () => {
+    return src("wwwroot/css/styles/*.css")
+        .pipe(cleanCSS())
+        .pipe(rename({ extname: ".min.css" }))
+        .pipe(dest("wwroot/css/"));
+}
+
+const defaultTask = () => {
+    watch("wwwroot/css/styles/*.css", minifyCSS);
+}
+
+exports.default = defaultTask;
+exports.minifyCSS = minifyCSS;
