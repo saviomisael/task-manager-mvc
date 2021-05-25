746c9f78a6ffd602de68ad894043da5b0b0421e9
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-05-25 14:30:19 -0300

    Add CategoryRepository as service

--- TaskManager/TaskManager.Web/Startup.cs
+++ TaskManager/TaskManager.Web/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TaskManager.Web.Repositories;
+using TaskManager.Web.Repositories.Contracts;
 
 namespace TaskManager.Web
 {
@@ -19,6 +21,8 @@ namespace TaskManager.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddTransient<ICategoryRepository, CategoryRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
