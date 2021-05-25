2c1d65918b4f9938030615fefecd23f482004511
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-05-25 14:23:11 -0300

    Add TaskController

---
+++ TaskManager/TaskManager.Web/Controllers/TaskController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskManager.Web.Repositories.Contracts;
+
+namespace TaskManager.Web.Controllers
+{
+    public class TaskController : Controller
+    {
+        private readonly ITaskRepository _taskRepository;
+
+        public TaskController(ITaskRepository taskRepository)
+        {
+            _taskRepository = taskRepository;
+        }
+    }
+}
