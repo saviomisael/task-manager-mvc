7f601ccd314a44a83c7d00314f8c24dd7a597c0a
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-05-25 14:16:10 -0300

    Refactor Category

--- TaskManager/TaskManager.Web/Models/Category.cs
+++ TaskManager/TaskManager.Web/Models/Category.cs
@@ -3,6 +3,6 @@
     public class Category
     {
         public int CategoryID { get; set; }
-        public string NameCategory { get; set; }
+        public string CategoryName { get; set; }
     }
 }
