3cb63c096b15862a509089af1d7802d4e9a5ced8
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-05-25 14:16:55 -0300

    Refactor model Task

--- TaskManager/TaskManager.Web/Models/Task.cs
+++ TaskManager/TaskManager.Web/Models/Task.cs
@@ -6,10 +6,10 @@ namespace TaskManager.Web.Models
     {
         public int TaskID { get; set; }
         public int CategoryFK { get; set; }
-        public string NameTask { get; set; }
-        public int PriorityTask { get; set; }
-        public string DescriptionTask { get; set; }
-        public DateTime DateTask { get; set; }
+        public string TaskName { get; set; }
+        public int TaskPriority { get; set; }
+        public string TaskDescription { get; set; }
+        public DateTime TaskDate { get; set; }
 
         public Category Category { get; set; }
     }
