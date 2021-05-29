45071e43dafa70fd3dbc0a153c6e42ae865950b6
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-05-29 14:53:32 -0300

    Add home_page script

---
+++ TaskManager/TaskManager.Web/wwwroot/js/scripts/home_page.js
@@ -0,0 +1,7 @@
+﻿let btnAddTask = document.querySelector(".menu-bar-home-page button");
+
+function goCreateTaskPage() {
+    window.location = "/tarefas/criar-tarefa"
+}
+
+btnAddTask.onclick = goCreateTaskPage;
