9150126e77361187144e6fb1d7ff5b494616a80a
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-05-29 14:37:31 -0300

    Add gulp-sourcemaps / minifyJS task

--- TaskManager/TaskManager.Web/gulpfile.js
+++ TaskManager/TaskManager.Web/gulpfile.js
@@ -1,6 +1,8 @@
 ﻿const { src, dest, watch } = require("gulp");
 const cleanCSS = require("gulp-clean-css");
 const rename = require("gulp-rename");
+const sourcemaps = require("gulp-sourcemaps");
+const uglify = require("gulp-uglify");
 
 const minifyCSS = () => {
     return src("wwwroot/css/styles/*.css")
@@ -9,8 +11,18 @@ const minifyCSS = () => {
         .pipe(dest("wwwroot/css/"));
 }
 
+const minifyJS = () => {
+    return src("wwwroot/js/scripts/*.js")
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(rename({ extname: ".min.js" }))
+        .pipe(sourcemaps.write())
+        .pipe(dest("wwwroot/js/"))
+}
+
 const defaultTask = () => {
     watch("wwwroot/css/styles/*.css", minifyCSS);
+    watch("wwwroot/js/scripts/*js", minifyJS);
 }
 
 exports.default = defaultTask;
