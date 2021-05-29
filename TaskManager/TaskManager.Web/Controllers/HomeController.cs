0d1070bc1dbaa5d98c75903cc3e7690445ed5a88
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-05-29 12:38:19 -0300

    Refactor HomeController

--- TaskManager/TaskManager.Web/Controllers/HomeController.cs
+++ TaskManager/TaskManager.Web/Controllers/HomeController.cs
@@ -1,14 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using TaskManager.Web.Models;
+using TaskManager.Web.Repositories.Contracts;
 
 namespace TaskManager.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ITaskRepository _taskRepository;
+
+        public HomeController(ITaskRepository taskRepository)
+        {
+            _taskRepository = taskRepository;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var allTasks = _taskRepository.ListAllTasks();
+
+            return View(allTasks);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
