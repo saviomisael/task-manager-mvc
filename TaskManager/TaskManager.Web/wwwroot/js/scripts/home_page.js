460f869e0846763c7484942261d72c8615ca00ab
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-05-31 15:52:58 -0300

    Add Index.cshtml HomeController styles

--- TaskManager/TaskManager.Web/wwwroot/js/scripts/home_page.js
+++ TaskManager/TaskManager.Web/wwwroot/js/scripts/home_page.js
@@ -4,4 +4,18 @@ function goCreateTaskPage() {
     window.location = "/tarefas/criar-tarefa"
 }
 
+const changePositionFooter = () => {
+    let tasks = document.querySelectorAll(".task-wrapper");
+
+    if (tasks.length <= 1 && screen.width < 1024) {
+        document.querySelector("footer").style.position = "fixed";
+    }
+
+    if (tasks.length > 3 && screen.width > 1024) {
+        document.querySelector("footer").style.position = "fixed";
+    }
+}
+
 btnAddTask.onclick = goCreateTaskPage;
+
+changePositionFooter();
