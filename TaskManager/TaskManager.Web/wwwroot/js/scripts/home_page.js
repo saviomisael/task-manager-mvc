05491958954cdf5a87d5b7807fdd2e99a6467a7b
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-06-08 19:41:57 -0300

    Fix home_page script

--- TaskManager/TaskManager.Web/wwwroot/js/scripts/home_page.js
+++ TaskManager/TaskManager.Web/wwwroot/js/scripts/home_page.js
@@ -10,12 +10,12 @@ const goCreateTaskPage = () => {
 const changePositionFooter = () => {
     let tasks = document.querySelectorAll(".task-wrapper");
 
-    if (tasks.length <= 1 && screen.width < 1024) {
-        document.querySelector("footer").style.position = "fixed";
+    if (tasks.length <= 2 && screen.width < 1024) {
+        document.querySelector(".footer").style.position = "fixed";
     }
 
-    if (tasks.length > 3 && screen.width > 1024) {
-        document.querySelector("footer").style.position = "fixed";
+    if (tasks.length < 3 && screen.width > 1024) {
+        document.querySelector(".footer").style.position = "fixed";
     }
 }
 
