1e24ba0377e7582202259eb139e2545b40102d8c
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-06-12 13:48:22 -0300

    Add MinTodayDate Validator

---
+++ TaskManager/TaskManager.Web/Validators/MinTodayDate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskManager.Web.Validators
+{
+    public class MinTodayDate : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var date = (DateTime)value;
+
+            return date >= DateTime.Now ? ValidationResult.Success : new ValidationResult("Você deve informar uma data maior ou igual a data de hoje.");
+        }
+    }
+}
