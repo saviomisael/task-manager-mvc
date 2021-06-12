35c8674f9245c5363c6997661f12aeb2b2d87fd4
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-06-12 14:45:15 -0300

    Add ErrorController

---
+++ TaskManager/TaskManager.Web/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskManager.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("error")]
+        public IActionResult ErrorStatusCode([FromQuery] int code)
+        {
+            return View(code);
+        }
+    }
+}
