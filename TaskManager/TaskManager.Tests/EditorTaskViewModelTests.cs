c617a32f3fcfd70e214ca5fc98fd1a32bfa9be0a
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-06-19 16:19:10 -0300

    Add ToViewModel_ShouldReturnEditorTaskViewModel test

--- TaskManager/TaskManager.Tests/EditorTaskViewModelTests.cs
+++ TaskManager/TaskManager.Tests/EditorTaskViewModelTests.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TaskManager.Web.Models;
+using TaskManager.Web.ViewModels;
+using Xunit;
 
 namespace TaskManager.Tests
 {
     public class EditorTaskViewModelTests
     {
+        [Fact]
+        public void ToViewModel_ShouldReturnEditorTaskViewModel()
+        {
+            var category = new Category("category 1");
+            var task = new Task("task 1", 1, "Descrition task 1", DateTime.Now, category);
+
+            var result = EditorTaskViewModel.ToViewModel(task);
+
+            Assert.IsType<EditorTaskViewModel>(result);
+
+            Assert.Equal(category.CategoryName, result.CategoryName);
+
+            Assert.Equal(task.TaskName, result.TaskName);
+            Assert.Equal(task.TaskPriority, result.TaskPriority);
+            Assert.Equal(task.TaskDescription, result.TaskDescription);
+            Assert.Equal(task.TaskDate, result.TaskDate);
+        }
     }
 }
