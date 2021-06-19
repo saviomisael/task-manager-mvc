68a40d3b2dc673edc69a31f4f67542c8e1ba2c12
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-06-19 14:46:51 -0300

    Add Index_ShouldReturnListTasks test

--- TaskManager/TaskManager.Tests/HomeControllerTests.cs
+++ TaskManager/TaskManager.Tests/HomeControllerTests.cs
@@ -1,10 +1,39 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using TaskManager.Web.Controllers;
+using TaskManager.Web.Models;
+using TaskManager.Web.Repositories.Contracts;
+using Xunit;
 
 namespace TaskManager.Tests
 {
     public class HomeControllerTests
     {
+        [Fact]
+        public void Index_ShouldReturnListTasks()
+        {
+            IList<Task> tasks = new List<Task>();
+            Category category1 = new Category(1, "Category 1");
+            Task task1 = new Task("Test 1", 1, "Description test 1", DateTime.Now, category1);
+            tasks.Add(task1);
+
+            Category category2 = new Category("Category 2");
+            Task task1 = new Task("Test 1", 1, "Description test 1", DateTime.Now, category1);
+            tasks.Add(task1);
+
+            Mock<ITaskRepository> taskRepositoryMock = new Mock<ITaskRepository>();
+
+            taskRepositoryMock.Setup(x => x.ListAllTasks()).Returns(tasks);
+
+            HomeController homeController = new HomeController(taskRepositoryMock.Object);
+
+            var result = homeController.Index();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+            IEnumerable<Task> modelViewResult = Assert.IsAssignableFrom<IEnumerable<Task>>(viewResult.Model);
+        }
     }
 }
