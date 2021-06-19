89acf8760a2b4563d2b30611aec4bdedc88fe6ce
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-06-19 16:00:49 -0300

    Add ErrorControllerTests

---
+++ TaskManager/TaskManager.Tests/ErrorControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskManager.Web.Controllers;
+using Xunit;
+
+namespace TaskManager.Tests
+{
+    public class ErrorControllerTests
+    {
+        [Fact]
+        public void ErrorStatusCode_ShouldReturnViewResult()
+        {
+            ErrorController errorController = new ErrorController();
+
+            var result = errorController.ErrorStatusCode(404);
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
