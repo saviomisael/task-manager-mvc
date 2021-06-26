a6ca787bbd4dea80da5461e34420fc38693d6504
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-06-26 15:30:54 -0300

    Pass CreateTask_ShouldReturnViewResult test

--- TaskManager/TaskManager.Tests/TaskControllerTests.cs
+++ TaskManager/TaskManager.Tests/TaskControllerTests.cs
@@ -1,10 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using TaskManager.Web.Controllers;
+using TaskManager.Web.Repositories.Contracts;
+using Xunit;
 
 namespace TaskManager.Tests
 {
     public class TaskControllerTests
     {
+        [Fact]
+        public void CreateTask_ShouldReturnViewResult()
+        {
+            var taskRepositoryMock = new Mock<ITaskRepository>();
+
+            var taskController = new TaskController(taskRepositoryMock.Object);
+
+            var result = taskController.CreateTask();
+
+            Assert.IsType<ViewResult>(result);
+        }
     }
 }
