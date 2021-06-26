1acae22a476a20135f5137a39063e6328a54bc67
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-06-26 18:42:36 -0300

    Revert to Refactor TaskControllerTests 29bbfcc6

--- TaskManager/TaskManager.Tests/TaskControllerTests.cs
+++ TaskManager/TaskManager.Tests/TaskControllerTests.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
-using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Moq;
 using System;
 using TaskManager.Web.Controllers;
@@ -37,6 +35,33 @@ namespace TaskManager.Tests
             var result = taskController.CreateTask();
 
             Assert.IsType<EditorTaskViewModel>(result.Model);
+        }
+
+        [Fact]
+        public void CreateTask_ShouldCreateTask_WhenViewModelIsValid()
+        {
+            var taskController = new TaskController(_taskRepositoryMock.Object);
+
+            var viewModel = new EditorTaskViewModel()
+            {
+                CategoryID = 1,
+                CategoryName = "Category 1",
+                TaskDate = DateTime.Today,
+                TaskName = "task 1",
+                TaskDescription = "Description task 1",
+                TaskPriority = 1
+            };
+
+            var model = EditorTaskViewModel.ToModel(viewModel);
+
+            _taskRepositoryMock.Setup(x => x.CreateTask(model)).Returns(true);
+
+            var action = taskController.CreateTask(viewModel);
+
+            var resultAction = Assert.IsType<RedirectToActionResult>(action);
+
+            Assert.Equal(nameof(HomeController.Index), resultAction.ActionName);
+            Assert.Equal("Home", resultAction.ControllerName);
         }
     }
 }
